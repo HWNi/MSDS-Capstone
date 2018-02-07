4ef65c0bfee95a2920591ee177faf0bbaea44552
Haowen Ni <nhw1068@hotmail.com>
2018-02-07 00:56:28 -0800

    Add functions for meta-path-based clustering

--- custom_setting.py
+++ custom_setting.py
@@ -1,8 +1,24 @@
-author_file = '/Users/zicong/Desktop/DataScience/Capstone/pubmed_data/Author_refined_simple_sample.csv'
-paper_author_file = '/Users/zicong/Desktop/DataScience/Capstone/pubmed_data/PaperAuthor_sample.csv'
+author_file = 'Author_refined_simple_sample.csv'
+paper_author_file = 'PaperAuthor_sample.csv'
+
 serialization_dir = '/Users/zicong/Desktop/DataScience/Capstone/pubmed_data/'
+
 name_statistics_file = 'name_statistics_seal'
 author_paper_stat_file = 'author_paper_stat_seal'
 name_instance_file = 'name_instance_seal'
 id_name_file = 'id_name_seal'
+
+duplicate_authors_file = "./result/duplicate_authors.csv"
 duplicate_groups_file = "duplicate_groups_seal"
+
+
+coauthor_matrix_file = 'coauthor_seal'
+covenue_matrix_file = 'covenue_seal'
+author_paper_matrix_file = 'author_paper_seal'
+author_affli_matrix_file = 'author_affli_matrix_seal'
+
+organization_count_threshold = 10000
+
+max_author = 2293837 # Author id
+max_paper = 2259021 # Max pmid
+max_affiliation = 83224 # Max affliation id
