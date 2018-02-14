fda65dec40011fd938d8bbed1efa33e4d2a25c42
tonyliang <lzctony@gmail.com>
2018-02-13 22:19:46 -0800

    Feature(post_process): add step 6

--- post_process.py
+++ post_process.py
@@ -1,35 +1,43 @@
 from meta_path import *
 
-def name_group_comparable(group, name_instance_dict, id_name_dict, name_statistics):
-    """Decide whether two groups of name instances are comparable"""
-    for author_A in group:
-        for author_B in group:
-            if author_A < author_B:
-                if not name_comparable(name_instance_dict[id_name_dict[author_A][0]], name_instance_dict[id_name_dict[author_B][0]], name_statistics, False):
-                    # print "\t\tConflicted name group: " + id_name_dict[author_A][0] + '\tv.s.\t' + id_name_dict[author_B][0]
-                    return False
-    return True
 
+def find_conflict_name(authors_duplicates_dict, name_instance_dict, id_name_dict, name_statistics):
+    """Find author ids whose duplicate author list contain conflicts in terms of their names"""
+    conflict_ids = set()
+    for (author_id, duplicate_group) in authors_duplicates_dict.iteritems():
+        if not name_group_comparable(duplicate_group, name_instance_dict, id_name_dict, name_statistics):
+            conflict_ids.add(author_id)
+    return conflict_ids
 
-def name_group_comparable_with_tolerence(group, group1, group2, name_instance_dict, id_name_dict, name_statistics):
-    """Decide whether two groups of name instances are comparable with certain tolerance"""
-    total = len(group1) * len(group2) + 0.0
-    disobey = 0
-    for author_A in group1:
-        for author_B in group2:
-            if not name_comparable(name_instance_dict[id_name_dict[author_A][0]], name_instance_dict[id_name_dict[author_B][0]], name_statistics, False):
-                disobey += 1
-    if min(len(group1), len(group2)) >= 4:
-        if disobey <= total * 0.2 or disobey <= min(len(group1), len(group2)) * 2:
-            return True
-        else:
-            return False
-    else:
-        if disobey <= min(len(group1), len(group2)) / 2:
-            return True
-        else:
-            return False
-
+# def name_group_comparable(group, name_instance_dict, id_name_dict, name_statistics):
+#     """Decide whether two groups of name instances are comparable"""
+#     for author_A in group:
+#         for author_B in group:
+#             if author_A < author_B:
+#                 if not name_comparable(name_instance_dict[id_name_dict[author_A][0]], name_instance_dict[id_name_dict[author_B][0]], name_statistics, False):
+#                     # print "\t\tConflicted name group: " + id_name_dict[author_A][0] + '\tv.s.\t' + id_name_dict[author_B][0]
+#                     return False
+#     return True
+#
+#
+# def name_group_comparable_with_tolerence(group, group1, group2, name_instance_dict, id_name_dict, name_statistics):
+#     """Decide whether two groups of name instances are comparable with certain tolerance"""
+#     total = len(group1) * len(group2) + 0.0
+#     disobey = 0
+#     for author_A in group1:
+#         for author_B in group2:
+#             if not name_comparable(name_instance_dict[id_name_dict[author_A][0]], name_instance_dict[id_name_dict[author_B][0]], name_statistics, False):
+#                 disobey += 1
+#     if min(len(group1), len(group2)) >= 4:
+#         if disobey <= total * 0.2 or disobey <= min(len(group1), len(group2)) * 2:
+#             return True
+#         else:
+#             return False
+#     else:
+#         if disobey <= min(len(group1), len(group2)) / 2:
+#             return True
+#         else:
+#             return False
 
 def merge_local_clusters(real_duplicate_groups, id_name_dict):
     """Merge local clusters.
@@ -213,3 +221,132 @@ def refine_result(authors_duplicates_dict, name_instance_dict, id_name_dict, nam
         if author_id in authors_duplicates_dict[author_id]:
             authors_duplicates_dict[author_id].remove(author_id)
 
+
+def final_filter(author_paper_stat, name_statistics, authors_duplicates_dict, name_instance_dict, id_name_dict, similarity_dict, metapaths):
+    """Filter obvious not confident names of duplicate authors for each author id."""
+    count = 0
+    for author_id in authors_duplicates_dict.iterkeys():
+        for duplicate_author_id in set(authors_duplicates_dict[author_id]):
+            if not name_comparable(name_instance_dict[id_name_dict[author_id][0]],
+                                   name_instance_dict[id_name_dict[duplicate_author_id][0]],
+                                   name_statistics,
+                                   False):
+                authors_duplicates_dict[author_id].remove(duplicate_author_id)
+                if author_id in authors_duplicates_dict[duplicate_author_id]:
+                    authors_duplicates_dict[duplicate_author_id].remove(author_id)
+                count += 1
+    print "\tRemoving " + str(count) + " author_ids from name comparison."
+
+    for (author_id, candi_ids) in authors_duplicates_dict.iteritems():
+        if id_name_dict[author_id][0] not in name_instance_dict:
+            continue
+        for id in name_instance_dict[id_name_dict[author_id][0]].author_ids:
+            if id in author_paper_stat and author_paper_stat[id] >= 1:
+                authors_duplicates_dict[author_id] = authors_duplicates_dict[author_id].union(authors_duplicates_dict[id])
+    for (author_id, candi_ids) in authors_duplicates_dict.iteritems():
+        for id in candi_ids:
+            if id in author_paper_stat and author_paper_stat[id] >= 1 and id_name_dict[id][0] in name_instance_dict:
+                authors_duplicates_dict[author_id] = authors_duplicates_dict[author_id].union(name_instance_dict[id_name_dict[id][0]].author_ids)
+    for (author_id, candi_ids) in authors_duplicates_dict.iteritems():
+        if id_name_dict[author_id][0] not in name_instance_dict:
+            continue
+        for id in name_instance_dict[id_name_dict[author_id][0]].author_ids:
+            if id in author_paper_stat and author_paper_stat[id] >= 1:
+                authors_duplicates_dict[author_id] = authors_duplicates_dict[author_id].union(authors_duplicates_dict[id])
+    for (author_id, candi_ids) in authors_duplicates_dict.iteritems():
+        for id in candi_ids:
+            if id in author_paper_stat and author_paper_stat[id] >= 1 and id_name_dict[id][0] in name_instance_dict:
+                authors_duplicates_dict[author_id] = authors_duplicates_dict[author_id].union(name_instance_dict[id_name_dict[id][0]].author_ids)
+    for (author_id, candi_ids) in authors_duplicates_dict.iteritems():
+        if id_name_dict[author_id][0] not in name_instance_dict:
+            continue
+        for id in name_instance_dict[id_name_dict[author_id][0]].author_ids:
+            if id in author_paper_stat and author_paper_stat[id] >= 1:
+                authors_duplicates_dict[author_id] = authors_duplicates_dict[author_id].union(authors_duplicates_dict[id])
+    for (author_id, candi_ids) in authors_duplicates_dict.iteritems():
+        for id in candi_ids:
+            if id in author_paper_stat and author_paper_stat[id] >= 1 and id_name_dict[id][0] in name_instance_dict:
+                authors_duplicates_dict[author_id] = authors_duplicates_dict[author_id].union(name_instance_dict[id_name_dict[id][0]].author_ids)
+    count = 0
+    for author_id in authors_duplicates_dict.iterkeys():
+        if len(authors_duplicates_dict[author_id]) == 1:
+            name_A = id_name_dict[author_id][0]
+            elements_A = name_A.split(' ')
+            #remove  A Dgh EF and Abc EF pairs
+            to_remove_set = set()
+            for id in authors_duplicates_dict[author_id]:
+                # if similarity_dict[tuple(sorted((id, author_id)))] > merge_threshold:
+                #     continue
+                name_B = id_name_dict[id][0]
+                elements_B = name_B.split(' ')
+                if ''.join(elements_A) == ''.join(elements_B):
+                    continue
+                if len(elements_A) > 2 and len(elements_A[0]) == 1 and len(elements_A[1]) > 1:
+                    if len(elements_B[0]) > 1 and len(elements_B) == 2 and elements_A[0][0] == elements_B[0][0] and elements_A[1][0] != elements_B[0][0]:
+                        to_remove_set.add(id)
+                        count += 1
+                        print '\t\tRemoving ' + name_B + ' from duplicates_set of ' + name_A
+                elif len(elements_B) > 2 and len(elements_B[0]) == 1 and len(elements_B[1]) > 1:
+                    if len(elements_A[0]) > 1 and len(elements_A) == 2 and elements_B[0][0] == elements_A[0][0] and elements_B[1][0] != elements_A[0][0]:
+                        to_remove_set.add(id)
+                        count += 1
+                        print '\t\tRemoving ' + name_B + ' from duplicates_set of ' + name_A
+            for id in to_remove_set:
+                authors_duplicates_dict[author_id].remove(id)
+        if len(authors_duplicates_dict[author_id]) <= 2:
+            if author_id not in id_name_dict or id_name_dict[author_id][0] not in name_instance_dict:
+                continue
+            name_A = id_name_dict[author_id][0]
+            elements_A = name_A.split(' ')
+            #remove  A Dgh EF and Abc EF pairs
+            to_remove_set = set()
+            flag = 0
+            for id in authors_duplicates_dict[author_id]:
+                # if similarity_dict[tuple(sorted((id, author_id)))] > merge_threshold:
+                #     continue
+                name_B = id_name_dict[id][0]
+                elements_B = name_B.split(' ')
+                if max(len(elements_A), len(elements_B)) >= 3 and min(len(elements_A), len(elements_B)) == 2:
+                    if tuple(sorted((id, author_id))) not in similarity_dict:
+                        similarity_dict[tuple(sorted((id, author_id)))] = max(compute_similarity_score(id, author_id, metapaths))
+                    if similarity_dict[tuple(sorted((id, author_id)))] <= merge_threshold:
+                        flag += 1
+                        stat1 = 0
+                        if name_statistics[elements_A[-1]] <= 20:
+                            continue
+                        for element_A in elements_A:
+                            if element_A in elements_B:
+                                stat1 += 1
+                            else:
+                                if element_A in ' '.join(elements_B[:-1]):
+                                    stat1 += 0.25
+                        stat2 = 0
+                        for element_B in elements_B:
+                            if element_B in elements_A:
+                                stat2 += 1
+                            else:
+                                if element_B in ' '.join(elements_A[:-1]):
+                                    stat2 += 0.25
+                        if max(stat1, stat2) <= 1.25:
+                            to_remove_set.add(id)
+                            count += 1
+                            # print min(stat1, stat2)
+                            print '\t\tRemoving ' + name_B + ' from duplicates_set of ' + name_A
+            if flag == len(authors_duplicates_dict[author_id]):
+                for id in to_remove_set:
+                    authors_duplicates_dict[author_id].remove(id)
+
+    print "\tFinish removing " + str(float(count)) \
+        + " unconfident names."
+
+    for author_id in authors_duplicates_dict.iterkeys():
+        if id_name_dict[author_id][1] == '':
+            authors_duplicates_dict[author_id] = set()
+    for author_id in authors_duplicates_dict.iterkeys():
+        if author_id in authors_duplicates_dict[author_id]:
+            authors_duplicates_dict[author_id].remove(author_id)
+        for id in set(authors_duplicates_dict[author_id]):
+            if id not in author_paper_stat or author_paper_stat[id] == 0:
+                authors_duplicates_dict[author_id].remove(id)
+        if author_id not in author_paper_stat or author_paper_stat[author_id] == 0:
+            authors_duplicates_dict[author_id] = set()
