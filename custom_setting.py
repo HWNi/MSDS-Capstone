61b7b1d4b67879f01764432940a63d65cc299d2b
Diana Zhang <dczhang@uw.edu>
2018-03-13 17:10:55 -0700

    format, add comment

--- custom_setting.py
+++ custom_setting.py
@@ -1,7 +1,8 @@
 # Raw data
-author_file = "./data_filtered/Author_refined.csv"
-paper_author_file = "./data_filtered/PaperAuthor_refined.csv"
-paper_file = './data_filtered/Paper.csv'
+author_file = "./data/Author_refined.csv"
+paper_author_file = "./data/PaperAuthor_refined.csv"
+paper_file = './data/Paper.csv'
+
 
 # Serialized files
 serialization_dir = './serialization/'
@@ -16,7 +17,6 @@ duplicate_authors_file = "./result/duplicate_authors.csv"
 duplicate_authors_full_name_file = "./result/duplicate_authors_fullname.csv"
 duplicate_groups_file = "duplicate_groups_seal"
 
-
 coauthor_matrix_file = 'coauthor_seal'
 covenue_matrix_file = 'covenue_seal'
 author_paper_matrix_file = 'author_paper_seal'
@@ -26,7 +26,7 @@ author_year_matrix_file = 'author_year_matrix_seal'
 author_word_matrix_file = 'author_word_seal'
 
 
-# Label Data Filtered
+# Aminer setting
 max_author = 3358 # Max Author id in sample
 max_paper = 1755784 # Max paper id
 max_conference = 1980 # Max conference id
@@ -34,7 +34,8 @@ word_title_count_threshold = 2000
 organization_count_threshold = 10000
 merge_threshold = 0.00000000001
 
-# PubMed
+
+# PubMed Setting 
 # max_author = 5232561 # Max Author id in sample
 # max_paper = 25928577 # Max paper id
 # max_affiliation = 83224 # Max affliation id
